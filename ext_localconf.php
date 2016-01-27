1386e621dc2797d7d64c928377e41b0f132c41f5
Wolfgang Klinger <wk@plan2.net>
2016-01-27 10:21:27 +0100

    [TASK] optimized extension (namespaces, composer vendor, better naming and code, …)

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,9 @@
 <?php
 
+defined('TYPO3_MODE') or die ('Access denied.');
+
 // Hook into TYPO3-Core Condition on device; Usage eg. [device = tablet], [device = smartPhone]
-if (TYPO3_MODE === 'FE') {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/div/class.t3lib_utility_client.php']['getDeviceType'][] = 'EXT:p2_mobiledetect/Classes/UserAgent.php:&Tx_P2Mobiledetect_UserAgent->getMobileDeviceType';
+if (TYPO3_MODE == 'FE') {
+	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/div/class.t3lib_utility_client.php']['getDeviceType'][] = 'EXT:p2_mobiledetect/Classes/Detect.php:Plan2net\MobileDetect\Detect->getMobileDeviceType';
 }
 
